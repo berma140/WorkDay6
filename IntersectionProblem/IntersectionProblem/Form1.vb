77878adfb9ad7d0f207f4649c520422a22613408
idsc3101 <idsc3101@gmail.com>
2014-10-06 15:25:22 -0500

    WorkDay 6 Complete

--- IntersectionProblem/IntersectionProblem/Form1.vb
+++ IntersectionProblem/IntersectionProblem/Form1.vb
@@ -6,28 +6,42 @@ Public Class Form1
         Dim Ast, Bst, Cst, Dst As String
         Dim A, B, C, D As Double
 
-        ' Read the input from the user
         Ast = txtA.Text
         Bst = txtB.Text
         Cst = txtC.Text
         Dst = txtD.Text
 
-        ' All input must be numbers
-        If Not (IsNumeric(Ast) And IsNumeric(Bst) And IsNumeric(Cst) _
-                And IsNumeric(Dst)) Then
+        If Not (IsNumeric(Ast) And IsNumeric(Bst) And IsNumeric(Cst) And IsNumeric(Dst)) Then
             MessageBox.Show("Please enter valid numbers", "Error!")
             Return
         End If
 
-        ' Convert to double
         A = CDbl(Ast)
         B = CDbl(Bst)
         C = CDbl(Cst)
         D = CDbl(Dst)
 
-        ' Please write your code here...
+        If (B < A Or D < C) Then
+            MessageBox.Show("Please enter valid numbers", "Error!")
+            Return
+        End If
 
-        txtResult.Text = "Not implemented yet"
+        txtResult.Text = FindIntersection(A, B, C, D)
     End Sub
+
+    Function FindIntersection(A As Double, B As Double, C As Double, D As Double) As String
+        Dim result As String
+        If (B < C) Or (D < A) Then
+            result = "No intersection"
+        Else
+            Dim begPoint, endPoint As Double
+            begPoint = Math.Max(A, C)
+            endPoint = Math.Min(B, D)
+
+            result = "There is an intersection from " & begPoint & " to " & endPoint
+        End If
+
+        Return result
+    End Function
 
 End Class
